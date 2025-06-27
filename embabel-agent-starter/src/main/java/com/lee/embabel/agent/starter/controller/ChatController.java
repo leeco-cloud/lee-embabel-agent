545a434ad9de6a5bed78c4e2cbaa186fb2a8ec21
lee <a6265391@qq.com>
2025-06-27 14:44:55 +0800

    init

--- embabel-agent-starter/src/main/java/com/lee/embabel/agent/starter/controller/ChatController.java
+++ embabel-agent-starter/src/main/java/com/lee/embabel/agent/starter/controller/ChatController.java
@@ -1,6 +1,5 @@
 package com.lee.embabel.agent.starter.controller;
 
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
