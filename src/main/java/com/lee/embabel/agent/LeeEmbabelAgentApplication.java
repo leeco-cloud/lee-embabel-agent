4f688d9013d3d011067aba2ed75b75d76cb75368
lee <a6265391@qq.com>
2025-06-27 13:46:39 +0800

    init

---
+++ src/main/java/com/lee/embabel/agent/LeeEmbabelAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lee.embabel.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeEmbabelAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeEmbabelAgentApplication.class, args);
+    }
+
+}
